926eb7e864c39f0d3c61f9496e7feff7411d4a8b
Nika Shamiladze <shamgoingzuck@gmail.com>
2022-01-07 18:35:26 +0400

    Prettier

--- getPositives/app.js
+++ getPositives/app.js
@@ -1,12 +1,12 @@
 function getPositives(x) {
-	let arr = [];
-	for(tx in x) {
-		if(x[tx]>=0) {
-    	arr = [...arr, x[tx]];
+  let arr = [];
+  for (tx in x) {
+    if (x[tx] >= 0) {
+      arr = [...arr, x[tx]];
     }
-	}
-  
+  }
+
   return arr;
 }
 
-console.log(getPositives([-1,-2,-35,1,2,3,4]))
+console.log(getPositives([-1, -2, -35, 1, 2, 3, 4]));
